bcefc8c87a41319959b2e9120bbd690ed193692c
Shubham Gaikwad <shubham.gaikwad@opcito.com>
2023-03-09 12:30:27 +0530

    fix: removes bearer from token with removeprefix

--- api/utils/token_required.py
+++ api/utils/token_required.py
@@ -3,26 +3,31 @@ from flask import request
 from firebase_admin import auth
 from ..services.auth_service import firebase_auth
 from ..models.user import User
+from ..exceptions import NoTokenException, AuthException, InvalidTokenException
 
 
 def token_required(roles=list(User.roles.keys())):
     """Decorator to require login and role based access control"""
+
     def decorator(func):
         @wraps(func)
         def wrap(*args, **kwargs):
             if not request.headers.get('Authorization'):
-                return {'message': 'No token provided'}, 400
+                # return {'message': 'No token provided'}, 400
+                raise NoTokenException()
 
             try:
-                token = request.headers.get('Authorization')[7:]
-
+                # token = request.headers.get('Authorization')[7:]
+                token = request.headers.get("Authorization").removeprefix("Bearer").strip()
+                # print(token)
                 custom_token = firebase_auth.auth.sign_in_with_custom_token(token)
                 user = auth.verify_id_token(custom_token['idToken'])
 
-                if user.get('role') not in roles:
-                    raise Exception("Unauthorized Access.")
             except Exception as e:
-                return {'message':'Invalid token provided.', "error": str(e)}, 400
+                raise InvalidTokenException()
+
+            if user.get('role') not in roles:
+                raise AuthException()
             return func(*args, **kwargs)
         return wrap
 
